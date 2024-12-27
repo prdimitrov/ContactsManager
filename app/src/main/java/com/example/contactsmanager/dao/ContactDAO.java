0f45df6bcba42a750bfb8d9e84bdcf3fcfdad2e0
prdimitrov <prdimitrov97@gmail.com>
2024-12-27 14:06:13 +0200

    Create Contact Data Access Object.

---
+++ app/src/main/java/com/example/contactsmanager/dao/ContactDAO.java
@@ -0,0 +1,24 @@
+package com.example.contactsmanager.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.contactsmanager.entity.Contact;
+
+import java.util.List;
+
+@Dao
+public interface ContactDAO {
+
+    @Insert
+    void insert(Contact contact);
+
+    @Delete
+    void delete(Contact contact);
+
+    @Query("SELECT * FROM contacts_table")
+    List<Contact> getAllContacts();
+
+}
